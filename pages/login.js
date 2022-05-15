cb5a386c4897bcbfca51bcedf74a4764bc449738
Lazarin Kroni <lazarinkroni@gmail.com>
2022-05-15 22:58:36 +0200

    first commit

---
+++ pages/login.js
@@ -0,0 +1,35 @@
+import { getProviders, signIn } from 'next-auth/react'
+
+function Login({ providers }) {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+      <img
+        className="mb-5 w-52"
+        src="https://www.geekslab.it/wp-content/uploads/2019/03/logo-spotify.png"
+        alt=""
+      />
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="rounded-lg bg-[#18D860] p-5 text-white"
+            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Login
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+
+  return {
+    props: {
+      providers,
+    },
+  }
+}
